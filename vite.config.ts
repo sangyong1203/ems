2838cf35e3bd8ba035497a2d85c430b47776bd2b
sangyong1203 <sangyong1203@outlook.com>
2026-07-23 14:41:16 +0900

    vite config설정, 컴포넌트 정리

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
+import svgLoader from 'vite-svg-loader'
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        Components({
+            dirs: ['src/shared/components'],
+            extensions: ['vue'],
+            deep: true,
+            dts: 'components.d.ts',
+        }),
+        svgLoader({
+            defaultImport: 'component',
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8000',
+                changeOrigin: true,
+            },
+        },
+    },
+})
